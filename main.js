a5d50cfcbf85218cf2456ac699bafc53546823e7
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2014-08-31 14:02:03 -0500

    Support compact and full wishlist formats
    Also changes total price style to be less obtrusive.

--- main.js
+++ main.js
@@ -1,7 +1,69 @@
 var $ = window.$;
 var accounting = window.accounting;
 
-var ITEMS_SELECTOR = '.g-items-section > [data-itemid]';
+var ITEMS_SELECTOR_FULL = '.g-items-section > [data-itemid]';
+var ITEMS_SELECTOR_COMPACT = '.g-compact-items tr + tr';
+var PRICE_PARENT_SELECTOR = '.top-nav-container .profile.a-declarative.top .profile-layout-aid-top';
+
+// if the selector finds things, returns a jQuery object, otherwise null. can
+// supply a $scope element as the first argument, and that jQuery element will
+// be used as the scope for the given selector(s). if more than one selector
+// argument is supplied, the first that returns something will be returned.
+var select = function ($scope) {
+  var selectorsStart = 0;
+  var context;
+
+  // if we were given a jQuery object as our first argument, scope to it
+  if (typeof $scope !== 'string') {
+    context = $scope;
+    selectorsStart = 1;
+  }
+
+  var $items;
+  for (var i = selectorsStart, length = arguments.length; i < length; i++) {
+    $items = $(arguments[i], context);
+    if ($items.length > 0) { break; }
+  }
+
+  return $items && $items.length > 0 ? $items : null;
+};
+
+// returns either the value of the given element if possible, otherwise its text
+var valOrText = function ($el) { return $el.val() || $el.text(); };
+
+// returns the items on the page, regardless of what view the wishlist is in
+var getItems = function () {
+  // each returns either a jQuery object of the items, or null
+  return select(ITEMS_SELECTOR_FULL, ITEMS_SELECTOR_COMPACT);
+};
+
+// given an item, parses it and returns JSON data for it
+var parseItem = function ($item) {
+  var $name = select($item, '[id^="itemName_"]', '.g-title a:first') || $();
+  var $price = select($item, '[id^="itemPrice_"]') || $();
+
+  var $want = select($item, '[id^="itemRequested_"]', '[name^="requestedQty"]') || $();
+  var $have = select($item, '[id^="itemPurchased_"]', '[name^="purchasedQty"]') || $();
+
+  var name = $name.text().trim();
+  var price = accounting.parse($price.text().trim());
+
+  // luckily, these show up even when not visible on the page!
+  var want = parseInt(valOrText($want), 10) || 1;
+  var have = parseInt(valOrText($have), 10) || 0;
+  var need = Math.max(0, want - have);
+
+  return {
+    name: name,
+
+    want: want,
+    have: have,
+    need: need,
+
+    price: price,
+    total_price: need * price,
+  };
+};
 
 // wait for the wishlist items to appear, then call the callback with the jQuery
 // object containing them.
@@ -14,17 +76,21 @@ var withItems = function (callback) {
 
     // the number of polls we need the count to be identical for and larger than
     // zero before we assume we've got all the items we're going to get.
-    var initialSameCount = 5;
+    var initialSameCount = 3;
     var sameCount = initialSameCount;
     var lastCount = 0;
 
     var wait = function () {
-      var $items = $(ITEMS_SELECTOR);
+      var $items = getItems();
 
       if ($items.length > 0) {
         if (sameCount <= 0) {
-          // we're done
-          return callback.call(null, $items);
+          // parse the items into an array of JSON objects
+          var items = [];
+          $items.each(function () { return items.push(parseItem($(this))); });
+
+          // call the callback with the parsed array
+          return callback.call(null, items);
         } else if ($items.length !== lastCount) {
           // we got a different number of items, reset the 'same' count and keep
           // waiting for it to settle down.
@@ -48,39 +114,8 @@ var withItems = function (callback) {
   }
 };
 
-
-// given an item, parses it and returns data for it
-var parseItem = function ($item) {
-  var name = $item.find('[id^="itemName_"]').text().trim() || '';
-  var price = accounting.parse($item.find('[id^="itemPrice_"]').text().trim());
-  var usedPrice = accounting.parse($item.find('.itemUsedAndNewPrice').text().trim());
-
-  // luckily, these show up even when not visible on the page!
-  var want = parseInt($item.find('[id^="itemRequested_"]').text().trim(), 10) || 1;
-  var have = parseInt($item.find('[id^="itemPurchased"]').text().trim(), 10) || 0;
-  var need = want - have;
-
-  return {
-    name: name,
-
-    want: want,
-    have: have,
-    need: need,
-
-    price: price,
-    used_price: usedPrice,
-
-    total_price: need * price,
-    total_used_price: need * usedPrice,
-  };
-};
-
-withItems(function ($items) {
-  var items = [];
-  $items.each(function () {
-    return items.push(parseItem($(this)));
-  });
-
+// run it!
+withItems(function (items) {
   var totalCount = items.reduce(function (count, cur) {
     return count + cur.need;
   }, 0);
@@ -89,21 +124,14 @@ withItems(function ($items) {
     return total + cur.total_price;
   }, 0);
 
-  console.log('items:', items);
-  console.log('totals:', totalCount, totalPrice);
-
-  // add a price beneath the wishlist title
-  var $priceParent = $('.top-nav-container .profile.a-declarative.top .profile-layout-aid-top');
-
   // use the styles/format of the shopping cart total
   var $total = $(
-    '<span id="wishlist-total" class="a-size-medium a-text-bold">' +
-      '<span>Subtotal (' + totalCount + ' item' + (totalCount === 1 ? '' : 's') + '):</span> ' +
-      '<span class="a-size-medium a-color-price sc-price sc-white-space-nowrap sc-price-sign">' +
-        accounting.formatMoney(totalPrice) +
-      '</span>' +
-    '</span>'
+    '<div id="wishlist-total">' +
+      '<span class="total-text">Subtotal (' + totalCount + ' item' + (totalCount === 1 ? '' : 's') + ')</span>: ' +
+      '<span class="total-price a-color-price">' + accounting.formatMoney(totalPrice) + '</span>' +
+    '</div>'
   );
 
-  $total.appendTo($priceParent);
+  // add the total to the DOM
+  $total.appendTo($(PRICE_PARENT_SELECTOR));
 });
