7eaf8d3150950ba8efcf306c72f16cf090f4ff8e
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2015-08-06 19:39:32 -0500

    Parse all pages of items
    We now screen scrape the wish list's pages for all possible items on the first
    load, then every few minutes thereafter. This lets us get all the items at once!
    
    We also scrape the current page several times a second so we can instantly
    reflect local updates the user has made.
    
    All of these item updates are stored in a global "database" of item id mapped to
    parsed item object, and we render the overall stats from this database.

--- main.js
+++ main.js
@@ -2,43 +2,39 @@ var $ = window.$;
 var accounting = window.accounting;
 
 // config
-var POLL_INTERVAL = 500;
+var ITEMS_SELECTOR = '.g-items-section > [data-itemid]';
+var WISH_LIST_ID = /\/wishlist\/(.+?)\//g.exec(window.location)[1];
+var ELEMENT_ID = 'wishlist-total';
 
-var ITEMS_SELECTOR_FULL = '.g-items-section > [data-itemid]';
-var ITEMS_SELECTOR_COMPACT = '.g-compact-items tr + tr'; // skip the header
-var TOTAL_ID = 'wishlist-total';
+// add a loading message that will be replaced later with our parsed info
+$(
+  '<div id="' + ELEMENT_ID + '">' +
+    '<i>Calculating wish list total…</i>' +
+  '</div>'
+).insertAfter('.top-nav-container .g-profile-stable.clip-text:first');
 
-// insert a dummy total element that will be updated with new content later
-var $total = $('<div/>')
-    .insertAfter('.top-nav-container .g-profile-stable.clip-text:first');
+// the database of items we're currently displaying. this is used so we can poll
+// the current page for changes instead of having to scrape the entire list
+// constantly.
+var ITEMS = {};
 
 // builds and returns the HTML for the total price element
 var tmplPriceElement = function (attrs) {
   return (
-    '<div id="' + TOTAL_ID + '">' +
+    '<div id="' + ELEMENT_ID + '">' +
       '<span class="total-text">Subtotal (' + attrs.total_count + ' item' + (attrs.total_count === 1 ? '' : 's') + ')</span>: ' +
       '<span class="total-price a-color-price">' + accounting.formatMoney(attrs.total_price) + '</span>' +
     '</div>'
   );
 };
 
-// if the selector finds things, returns a jQuery object, otherwise null. can
-// supply a $scope element as the first argument, and that jQuery element will
-// be used as the scope for the given selector(s). if more than one selector
-// argument is supplied, the first that returns something will be returned.
+// given a `$scope` jQuery element and some selectors, returns a jQuery object
+// matching the first selector that finds something, otherwise null if no
+// selectors find anything.
 var select = function ($scope) {
-  var selectorsStart = 0;
-  var context;
-
-  // if we were given a jQuery object as our first argument, scope to it
-  if (typeof $scope !== 'string') {
-    context = $scope;
-    selectorsStart = 1;
-  }
-
   var $items;
-  for (var i = selectorsStart, length = arguments.length; i < length; i++) {
-    $items = $(arguments[i], context);
+  for (var i = 1, length = arguments.length; i < length; i++) {
+    $items = $(arguments[i], $scope);
     if ($items.length > 0) { break; }
   }
 
@@ -48,14 +44,10 @@ var select = function ($scope) {
 // returns either the value of the given element if possible, otherwise its text
 var valOrText = function ($el) { return $el.val() || $el.text(); };
 
-// returns the items on the page, regardless of what view the wishlist is in
-var getItemElements = function () {
-  // each returns either a jQuery object of the items, or null
-  return select(ITEMS_SELECTOR_FULL, ITEMS_SELECTOR_COMPACT);
-};
-
-// given an item, parses it and returns JSON data for it
+// given a jQuery object for an item, parses it and returns JSON data for it
 var parseItem = function ($item) {
+  var id = $item.attr('data-itemid');
+
   var $name = select($item, '[id^="itemName_"]', '.g-title a:first') || $();
   var $price = select($item, '[id^="itemPrice_"]') || $();
 
@@ -74,61 +66,156 @@ var parseItem = function ($item) {
   var have = parseInt(valOrText($have), 10) || 0;
   var need = Math.max(0, want - have);
 
+  // set all counts to zero if the item has been deleted. this means the totals
+  // we get will be 0, meaning the item won't affect overall calculations.
+  if ($item.is('.g-item-sortable-removed')) {
+    want = 0;
+    have = 0;
+    need = 0;
+  }
+
   return {
+    id: id,
     name: name,
 
-    want: want,
-    have: have,
-    need: need,
+    counts: {
+      have: have,
+      need: need,
+      want: want
+    },
 
     price: price,
     total_price: need * price
   };
 };
 
-// given a jQuery object of items, parses them into an array of parsed items.
-// also includes totals for count and price and stores them on the array object.
-var parseItems = function ($items) {
+// given a jQuery object representing a single wish list page, parses it into an
+// array of individual JSON wish list items.
+var parsePage = function ($page) {
   // turn the items jQuery object into an array of parsed items
+  var $items = select($page, ITEMS_SELECTOR);
+  var items = $items.map(function () {
+    // deleted items get parsed as having no price, which effectively deletes
+    // them from the database (a useful thing so we don't have to do a real
+    // delete).
+    return parseItem($(this));
+  });
+
+  // turn it into a proper array, not a jQuery object
+  return Array.prototype.slice.call(items);
+};
+
+// given an array of wish list pages, parses the items out of each of them and
+// returns a JSON object representing the overall wish list.
+var parseWishList = function (pages) {
   var items = [];
-  if ($items) {
-    $items.each(function () {
-      // skip removed items
-      if ($(this).is(':not(.g-item-sortable-removed)')) {
-        items.push(parseItem($(this)));
-      }
-    });
-  }
-
-  // the total number of needed items, taking quantity into account
-  items.total_count = items.reduce(function (count, cur) {
-    // only count items that we found a price for, i.e. that were available on
-    // Amazon and not just from other retailers.
-    return count + (cur.price ? cur.need : 0);
-  }, 0);
-
-  // the total price of all the items, taking the quantity into account
-  items.total_price = items.reduce(function (total, cur) {
-    return total + cur.total_price;
-  }, 0);
-
+  pages.forEach(function ($page) {
+    items.push.apply(items, parsePage($page));
+  });
   return items;
 };
 
-// grabs all items, parses them, and returns the result
-var getParsedItems = function () { return parseItems(getItemElements()); };
+// given a list of items, calculates overall price and count for them
+var calculateItemTotals = function (items) {
+  // the total number of needed items, taking quantity into account
+  var totalCount = items.reduce(function (count, item) {
+    // only count items that we found a price for, i.e. that were available on
+    // Amazon and not just from other retailers.
+    return count + (item.price ? item.counts.need : 0);
+  }, 0);
 
-// check for new items continuously. this is the simplest way to be correct :(
-var lastTotal = null;
-setInterval(function () {
-  var items = getParsedItems();
+  // the total price of all the items, taking the quantity into account
+  var totalPrice = items.reduce(function (total, item) {
+    return total + item.total_price;
+  }, 0);
 
-  // if the total has changed, update the DOM
-  if (items.total_price !== lastTotal) {
-    lastTotal = items.total_price;
-    $total.replaceWith(tmplPriceElement({
-      total_count: items.total_count,
-      total_price: items.total_price
-    }));
-  }
-}, POLL_INTERVAL);
+  return {
+    total_count: totalCount,
+    total_price: totalPrice
+  };
+};
+
+// download all available pages for the given wish list and return them to the
+// callback as an array of jQuery objects.
+var fetchWishListPages = function (id, callback, pages) {
+  // pages that have been fetched so far
+  pages = pages || [];
+  var pageNumber = pages.length + 1;
+
+  console.log('fetching wish list page ' + pageNumber + '...');
+
+  var url = window.location.pathname.split(id)[0] + id + '?' + $.param({
+    page: pageNumber
+  });
+
+  // fetch the given page of results, parse them, and call the callback with
+  // them.
+  $.ajax({
+    url: url,
+    method: 'GET'
+  }).done(function (data) {
+    // parse the downloaded data into a jQuery object and add it to our
+    // accumulated pages list.
+    var $page = $(data);
+    pages = pages.concat([$page]);
+
+    // check whether there's an accessible "Next" link
+    var $next = $page.find('#wishlistPagination .a-last:not(.a-disabled) a:first');
+    var hasNextPage = $next.length > 0;
+
+    // if we have another page to download, continue, otherwise return
+    if (hasNextPage) {
+      fetchWishListPages(id, callback, pages);
+    } else {
+      callback.call(null, pages);
+    }
+  }).fail(function () {
+    // log an error and return nothing if we failed
+    console.error('Failed to fetch page!', arguments);
+    callback.call(null, []);
+  });
+};
+
+// update the existing items with
+var updateDatabaseFromItems = function (items) {
+  items.forEach(function (item) {
+    ITEMS[item.id] = item;
+  });
+};
+
+// given a wish list id, downloads all its pages, parses the items, adds them to
+// the global database, then calls the given callback.
+var updateDatabaseFromAPI = function (id, callback) {
+  fetchWishListPages(id, function (pages) {
+    var items = parseWishList(pages);
+    updateDatabaseFromItems(items);
+
+    // notify that we've finished adding the items to the global database
+    callback.call(null);
+  });
+};
+
+var renderItemsFromDatabase = function () {
+  // collect all the items into a single list
+  var items = [];
+  Object.keys(ITEMS).forEach(function (key) { items.push(ITEMS[key]); });
+
+  var totals = calculateItemTotals(items);
+  $('#' + ELEMENT_ID).replaceWith(tmplPriceElement(totals));
+};
+
+// populate the items database with an initial full download. once we've
+// finished the initial download, start doing screen-scrape updates too.
+updateDatabaseFromAPI(WISH_LIST_ID, function () {
+  // continuously check the current page for user changes to add to the database
+  setInterval(function () {
+    var items = parsePage($('html'));
+    updateDatabaseFromItems(items);
+    renderItemsFromDatabase();
+  }, 250);
+
+  // periodically do an update from the API in case other pages have changed
+  setInterval(function () {
+    updateDatabaseFromAPI(WISH_LIST_ID);
+  }, 3 * 60 * 1000);
+});
