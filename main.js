3698052616f8e367c6bba7156d5cb4299a6393ef
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2016-08-20 15:21:33 -0500

    Refactor everything to ES6
    Chrome's support is now almost entirely complete. Yay!

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const ELEMENT_ID = 'wishlist-total';
 const ITEMS = {};
 
 // Select the given elements from the document and return them as an array.
-const $ = function (selectorOrElement, selector) {
+const $ = (selectorOrElement, selector) => {
   let el = selectorOrElement;
   if (typeof selectorOrElement === 'string') {
     el = document;
@@ -24,10 +24,10 @@ const $ = function (selectorOrElement, selector) {
 // Given a `$scope` DOM element and some selectors, returns an array of DOM
 // nodes matching the first selector that finds something, otherwise an empty
 // array if no selectors find anything.
-const $$ = function ($scope) {
+const $$ = ($scope, ...selectors) => {
   let $items = [];
-  for (let i = 1; i < arguments.length; i++) {
-    $items = $($scope, arguments[i]);
+  for (let i = 0, len = selectors.length; i < len; i++) {
+    $items = $($scope, selectors[i]);
     if ($items.length > 0) { break; }
   }
 
@@ -36,126 +36,93 @@ const $$ = function ($scope) {
 
 // Information about the current locale, including how to translate the
 // different text in the application.
-const LOCALE = (function () {
-  const localizationData = {
-    '.ca': {
-      currency_code: 'CAD',
-      loading_text: 'Calculating wish list total…',
-      subtotal_text: function (n) {
-        return `Subtotal (${n} item${(n === 1 ? '' : 's')})`;
-      },
-    },
+const LOCALE = (() => {
+  const englishTemplate = Object.freeze({
+    loading_text: 'Calculating wish list total…',
+    subtotal_text: (n) => `Subtotal (${n} item${(n === 1 ? '' : 's')})`,
+  });
 
+  const spanishTemplate = Object.freeze({
+    loading_text: 'Cálculo lista de artículos deseados total de…',
+    subtotal_text: (n) => `Subtotal (${n} producto${(n === 1 ? '' : 's')})`,
+  });
+
+  const localizationData = {
+    // ENGLISH
+    '.ca': Object.assign({
+      currency_code: 'CAD',
+    }, englishTemplate),
+
+    '.co.uk': Object.assign({
+      currency_code: 'GBP',
+    }, englishTemplate),
+
+    '.com': Object.assign({
+      currency_code: 'USD',
+    }, englishTemplate),
+
+    '.com.au': Object.assign({
+      currency_code: 'AUD',
+    }, englishTemplate),
+
+    '.ie': Object.assign({
+      currency_code: 'EUR',
+    }, englishTemplate),
+
+    '.in': Object.assign({
+      currency_code: 'INR',
+    }, englishTemplate),
+
+    // SPANISH
+    '.com.mx': Object.assign({
+      currency_code: 'MXN',
+    }, spanishTemplate),
+
+    '.es': Object.assign({
+      currency_code: 'EUR',
+    }, spanishTemplate),
+
+    // OTHER
     '.cn': {
       currency_code: 'CNY',
       loading_text: '计算愿望清单总…',
-      subtotal_text: function (n) {
-        return `小计 (${n} 件商品)`;
-      },
+      subtotal_text: (n) => `小计 (${n} 件商品)`,
     },
 
     '.co.jp': {
       currency_code: 'JPY',
       loading_text: 'ウィッシュリスト合計を計算…',
-      subtotal_text: function (n) {
-        return `小計 (${n} 商品)`;
-      },
-    },
-
-    '.co.uk': {
-      currency_code: 'GBP',
-      loading_text: 'Calculating wish list total…',
-      subtotal_text: function (n) {
-        return `Subtotal (${n} item${(n === 1 ? '' : 's')})`;
-      },
-    },
-
-    '.com': {
-      currency_code: 'USD',
-      loading_text: 'Calculating wish list total…',
-      subtotal_text: function (n) {
-        return `Subtotal (${n} item${(n === 1 ? '' : 's')})`;
-      },
-    },
-
-    '.com.au': {
-      currency_code: 'AUD',
-      loading_text: 'Calculating wish list total…',
-      subtotal_text: function (n) {
-        return `Subtotal (${n} item${(n === 1 ? '' : 's')})`;
-      },
+      subtotal_text: (n) => `小計 (${n} 商品)`,
     },
 
     '.com.br': {
       currency_code: 'BRL',
       loading_text: 'Calculando lista de desejos total de…',
-      subtotal_text: function (n) {
-        return `Subtotal (${n} item${(n === 1 ? '' : 's')})`;
-      },
-    },
-
-    '.com.mx': {
-      currency_code: 'MXN',
-      loading_text: 'Cálculo lista de artículos deseados total de…',
-      subtotal_text: function (n) {
-        return `Subtotal (${n} producto{(n === 1 ? '' : 's')})`;
-      },
+      subtotal_text: (n) => `Subtotal (${n} iten${(n === 1 ? '' : 's')})`,
     },
 
     '.de': {
       currency_code: 'EUR',
       loading_text: 'Berechnung Wunschzettel insgesamt…',
-      subtotal_text: function (n) {
-        return `Summe (${n} Artikel)`;
-      },
-    },
-
-    '.es': {
-      currency_code: 'EUR',
-      loading_text: 'Cálculo lista de artículos deseados total de…',
-      subtotal_text: function (n) {
-        return `Subtotal (${n} producto${(n === 1 ? '' : 's')})`;
-      },
+      subtotal_text: (n) => `Summe (${n} Artikel)`,
     },
 
     '.fr': {
       currency_code: 'EUR',
       loading_text: 'Calcul liste de souhaits totale…',
-      subtotal_text: function (n) {
-        return `Sous-total (${n} article${(n === 1 ? '' : 's')}))`;
-      },
-    },
-
-    '.ie': {
-      currency_code: 'EUR',
-      loading_text: 'Calculating wish list total…',
-      subtotal_text: function (n) {
-        return `Subtotal (${n} item${(n === 1 ? '' : 's')})`;
-      },
-    },
-
-    '.in': {
-      currency_code: 'INR',
-      loading_text: 'Calculating wish list total…',
-      subtotal_text: function (n) {
-        return `Subtotal (item${(n === 1 ? '' : 's')})`;
-      },
+      subtotal_text: (n) => `Sous-total (${n} article${(n === 1 ? '' : 's')}))`,
     },
 
     '.it': {
       currency_code: 'EUR',
       loading_text: 'Calcolo lista dei desideri totale…',
-      subtotal_text: function (n) {
-        return `Totale provvisorio (${n} articol${(n === 1 ? 'o' : 'i')})`;
-      },
+      subtotal_text: (n) => `Totale provvisorio (${n} articol${(n === 1 ? 'o' : 'i')})`,
     },
 
     '.nl': {
       currency_code: 'EUR',
       loading_text: 'Berekenen wens lijst totaal…',
-      subtotal_text: function (n) {
-        return `Summe (${n} Artikel)`;
-      },
+      subtotal_text: (n) => `Summe (${n} Artikel)`,
     },
   };
 
@@ -171,11 +138,11 @@ const LOCALE = (function () {
 
   // Default to USA, for lack of a better option.
   return localizationData['.com'];
-}());
+})();
 
 // Given an element and a selector, returns the closest matching parent node
 // (including the element itself), or `null` if none matches.
-const closest = function ($el, selector) {
+const closest = ($el, selector) => {
   let $current = $el;
   while ($current && !$current.matches(selector)) {
     $current = $current.parentElement;
@@ -184,20 +151,31 @@ const closest = function ($el, selector) {
   return $current || null;
 };
 
+// Escapes the given string for direct use as HTML. The result is _not_ suitable
+// for use in script tags or style blocks!
+const escapeHTML = (s) => (s || '').replace(/[<>]/g, (c) => {
+  switch (c) {
+  case '<':
+    return '&lt;';
+  case '>':
+    return '&gt;';
+  default:
+    throw new Error(`Invalid HTML escape character: '${c}'`);
+  }
+});
+
 // A tagged template function for building a DOM element. Returns an array of
 // constructed DOM elements, possibly containing only a single item if only a
 // single item was specified in the string.
-const DOM = function (strings) {
-  const values = Array.prototype.slice.call(arguments, 1);
-  strings = Array.prototype.slice.call(strings);
-
+const DOM = (strings, ...values) => {
   const parts = [];
-  while (strings.length > 0 || values.length > 0) {
-    const nextString = strings.shift();
-    const nextValue = values.shift();
 
-    if (nextString) { parts.push(nextString); }
-    if (nextValue) { parts.push(nextValue); }
+  for (let i = 0, len = Math.max(strings.length, values.length); i < len; i++) {
+    const s = strings[i];
+    const v = values[i];
+
+    if (s) { parts.push(s); }
+    if (v) { parts.push(escapeHTML(v)); }
   }
 
   const el = document.createElement('div');
@@ -207,7 +185,7 @@ const DOM = function (strings) {
 };
 
 // Build and return a DOM element for our element.
-const buildPriceElement = function (attrs) {
+const buildPriceElement = (attrs) => {
   attrs = attrs || {};
 
   if (attrs.loading) {
@@ -236,7 +214,7 @@ const buildPriceElement = function (attrs) {
 };
 
 // Finds the id of the currently-viewed wish list and returns it as a string.
-const getCurrentWishListId = function () {
+const getCurrentWishListId = () => {
   const $state = $('script[type="a-state"][data-a-state*="navState"]');
 
   if ($state.length > 0) {
@@ -249,14 +227,14 @@ const getCurrentWishListId = function () {
 
 // Returns either the value of the given element if possible, otherwise its
 // text.
-const valOrText = function ($el) { return $el.value || $el.innerText; };
+const valOrText = ($el) => { return $el.value || $el.innerText; };
 
 // Turns a currency string into a single floating point number. If the number
 // has thousands separators _or_ uses commas instead of periods to separate the
 // fraction, we normalize the number to a decimal fraction first. If multiple
 // currency values are included in the string, an average of all of them is
 // returned.
-const parseCurrency = function (s) {
+const parseCurrency = (s) => {
   // Pare down our string to include only digits, commas, periods, and single
   // literal spaces instead of multiple whitespace characters.
   s = (s || '')
@@ -267,7 +245,7 @@ const parseCurrency = function (s) {
   // Get all the possible numbers in the string.
   const parts = s.split(/\s/g);
 
-  const values = parts.map(function (part) {
+  const values = parts.map((part) => {
     // Use the number's final non-digit character to detect whether it's
     // formatted with periods (1,234.56) or commas (1.234,56).
     const commaSeparated = part.replace(/[^.,]+/g, '').endsWith(',');
@@ -286,13 +264,13 @@ const parseCurrency = function (s) {
   });
 
   // Calculate and return the average of the parsed values.
-  return values.reduce(function (sum, value) {
+  return values.reduce((sum, value) => {
     return sum + value;
   }, 0) / parts.length;
 };
 
 // Given a DOM node for an item, parses it and returns JSON data for it.
-const parseItem = function ($item) {
+const parseItem = ($item) => {
   // Each item element hopefully has an id like "id_ITEMIDSTUFF"
   const id = $item.id.split('_')[1];
 
@@ -353,9 +331,9 @@ const parseItem = function ($item) {
 
 // Given a DOM document representing a single wish list page, parses it into an
 // array of individual JSON wish list items.
-const parsePage = function ($page) {
+const parsePage = ($page) => {
   // Parse all items into an array of JSON objects.
-  return $($page, '.g-items-section [id^="item_"]').map(function ($item) {
+  return $($page, '.g-items-section [id^="item_"]').map(($item) => {
     // Deleted items get parsed as having no price, which effectively deletes
     // them from the database (a useful thing so we don't have to do a real
     // delete).
@@ -365,25 +343,25 @@ const parsePage = function ($page) {
 
 // Given an array of wish list pages, parses the items out of each of them and
 // returns a JSON object representing the overall wish list.
-const parseWishList = function (pages) {
+const parseWishList = (pages) => {
   const items = [];
-  pages.forEach(function ($page) {
+  pages.forEach(($page) => {
     items.push.apply(items, parsePage($page));
   });
   return items;
 };
 
 // given a list of items, calculates overall price and count for them
-const calculateItemTotals = function (items) {
+const calculateItemTotals = (items) => {
   // the total number of needed items, taking quantity into account
-  const totalCount = items.reduce(function (count, item) {
+  const totalCount = items.reduce((count, item) => {
     // only count items that we found a price for, i.e. that were available on
     // Amazon and not just from other retailers.
     return count + (item.price ? item.counts.need : 0);
   }, 0);
 
   // the total price of all the items, taking the quantity into account
-  const totalPrice = items.reduce(function (total, item) {
+  const totalPrice = items.reduce((total, item) => {
     return total + item.total_price;
   }, 0);
 
@@ -428,18 +406,18 @@ const fetchWishListPages = (id, callback, pages) => {
 };
 
 // Add the given items to our wish list database, overwriting any existing ones.
-const updateDatabaseFromItems = function (items) {
-  items.forEach(function (item) {
+const updateDatabaseFromItems = (items) => {
+  items.forEach((item) => {
     ITEMS[item.id] = item;
   });
 };
 
 // Given a wish list id, downloads all its pages, parses the items, adds them to
 // the global database, then calls the given callback.
-const updateDatabaseFromAPI = function (id, callback) {
-  callback = callback || function () {};
+const updateDatabaseFromAPI = (id, callback) => {
+  callback = callback || (() => {});
 
-  fetchWishListPages(id, function (pages) {
+  fetchWishListPages(id, (pages) => {
     const items = parseWishList(pages);
     updateDatabaseFromItems(items);
 
@@ -451,9 +429,9 @@ const updateDatabaseFromAPI = function (id, callback) {
 // Render all the items in the current database, assuming the totals have
 // changed since the last render.
 let PREVIOUS_HASH = null;
-const renderItemsFromDatabase = function () {
+const renderItemsFromDatabase = () => {
   // Collect all the items into a single list.
-  const items = Object.keys(ITEMS).map(function (k) { return ITEMS[k]; });
+  const items = Object.keys(ITEMS).map((k) => { return ITEMS[k]; });
 
   // Render our items into the DOM, assuming it still exists!
   const totals = calculateItemTotals(items);
@@ -473,28 +451,15 @@ $('body')[0].appendChild(buildPriceElement({ loading: true })[0]);
 // Populate the items database with an initial full download. Once we've
 // finished the initial download, start doing screen-scrape updates too.
 const WISH_LIST_ID = getCurrentWishListId();
-updateDatabaseFromAPI(WISH_LIST_ID, function () {
+updateDatabaseFromAPI(WISH_LIST_ID, () => {
   // Continuously check the current page for user changes to add to the
   // database.
-  setInterval(function () {
+  setInterval(() => {
     const items = parsePage(document.documentElement);
     updateDatabaseFromItems(items);
     renderItemsFromDatabase();
   }, 100);
 
   // Periodically do an update from the API in case other pages have changed.
-  let previousItemCount = null;
-  setInterval(function () {
-    updateDatabaseFromAPI(WISH_LIST_ID, function () {
-      // If the number of items in the list changes, reload the list so we can
-      // tell!
-      const itemCount = Object.keys(ITEMS).length;
-      if (previousItemCount === null) {
-        previousItemCount = itemCount;
-      } else if (previousItemCount !== itemCount) {
-        window.location.reload(true);
-        return;
-      }
-    });
-  }, 5 * 1000);
+  setInterval(() => updateDatabaseFromAPI(WISH_LIST_ID), 5 * 1000);
 });
